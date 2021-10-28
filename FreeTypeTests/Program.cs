a538634ec01dbab97853b8f8ebb790af72d32240
Bird Egop <sampletext32@bk.ru>
2021-10-28 05:23:37 +0300

    FreeType Tests

---
+++ FreeTypeTests/Program.cs
@@ -0,0 +1,19 @@
+﻿using SharpFont;
+
+namespace FreeTypeTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // NOTE: We need to copy a pre-built freetype6.dll inside the build folder 
+            Library library = new Library();
+            Face face = new Face(library, "assets/Font/arial.ttf");
+            face.SetPixelSizes(0, 48);
+            face.LoadChar('a', LoadFlags.Render, LoadTarget.Normal);
+
+            var faceGlyph = face.Glyph;
+            int a = 5;
+        }
+    }
+}
